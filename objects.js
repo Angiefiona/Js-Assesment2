abdd828968dcac635bc4697069284617fdcdd4c8
Angiefiona <angellamumoh@gmail.com>
2023-05-01 08:45:09 +0300

    first commit

---
+++ objects.js
@@ -0,0 +1,42 @@
+const books = [
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+    isAvailable: true },
+    { title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+    isAvailable: false },
+    { title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+    1866, isAvailable: true },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+    isAvailable: false },
+    { title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+    ];
+// Write a JavaScript program that performs the following tasks:
+// 1. Create a function called getAvailableBooks that returns an array of all available
+// books.
+function getAvailableBooks()
+let book = book1
+console.log(book)
+// 2. Create a function getBooksByAuthor that takes an author's name as an argument and
+// returns an array of all books by that author.
+// function getBooksByAuthor(name)
+// let book1 = book.getBooksByAuthor
+
+
+
+// 3. Create a function addNewBook that takes a book object as an argument and adds it
+// to the library, ensuring that the new book has all required properties (title, author,
+// publicationYear, and isAvailable).
+function addNewBook()
+let newBook = books.push(title,author,publicationYear,isAvailable) 
+console.log(addNewBook("pyschology", "Tindi Terry", 1990,))
+console.log(newBook(isAvailable))
+// 4. Create a function checkoutBook that takes a book title as an argument and changes
+// the book's isAvailable property to false. If the book is not found in the library, the
+// function should return a message indicating that the book is not available.
+function checkoutBook(title)
+let title = 0
+for title in books{
+    
+}
+// 5. Create a function returnBook that takes a book title as an argument and changes the
+// book's isAvailable property to true. If the book is not found in the library, the function
+// should return a message indicating that the book does not belong to the library.
